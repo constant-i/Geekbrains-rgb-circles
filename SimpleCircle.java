d08da181cfcf61e239f575b35c543158c46de41f
constant-i <constant-i@yandex.ru>
2019-02-03 21:18:08 +0300

    в класс-предок SimpleCirkle (супер-класс) для класов "MainCircle", класс "круг-еда" и класс "круг-враг" добавлены базовые методы и поля класса

--- SimpleCircle.java
+++ SimpleCircle.java
@@ -1,5 +1,25 @@
 package com.hfad.rgbcircles;
 
 public class SimpleCircle {
-    
+    protected int x;
+    protected int y;
+    protected int radius;
+
+    public SimpleCircle(int x, int y, int radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
 }
