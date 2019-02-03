b311182e0c9d54586b282f722de3892f0a9a75a5
constant-i <constant-i@yandex.ru>
2019-02-03 21:15:44 +0300

    класс MainCircle стал потомком класса SimpleCircle, и из него ушли все базовые методы в класс-предок (супер-класс SimpleCircle)

--- MainCircle.java
+++ MainCircle.java
@@ -1,29 +1,11 @@
 package com.hfad.rgbcircles;
 
-public class MainCircle {
+public class MainCircle extends SimpleCircle {
     public static final int INIT_RADIUS = 50;
     public static final int MAIN_SPEED = 30;
-    private int x;
-    private int y;
-    private int radius;
-
 
     public MainCircle(int x, int y) {
-        this.x = x;
-        this.y = y;
-        this.radius = INIT_RADIUS;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public int getRadius() {
-        return radius;
+        super(x, y, INIT_RADIUS);
     }
 
 
