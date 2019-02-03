87505ffad85d8f030417f4f160f319b8757b2f92
constant-i <constant-i@yandex.ru>
2019-02-03 18:24:24 +0300

    класс GameManager после 5 урока, на середине 6

---
+++ GameManager.java
@@ -0,0 +1,43 @@
+package com.hfad.rgbcircles;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+public class GameManager {
+    private MainCircle mainCircle;
+    private CanvasView canvasView;
+    private static int width;
+    private static int height;
+
+
+    public GameManager(CanvasView canvasView, int w, int h) {
+        this.canvasView = canvasView;
+        width = w;
+        height = h;
+        initMainCircle();
+    }
+
+    public static int getWidth() {
+        return width;
+    }
+
+    public static int getHeight() {
+        return height;
+    }
+
+    private void initMainCircle() {
+        mainCircle = new MainCircle(width/2,height/2);
+    }
+
+    public void onDraw() {
+        canvasView.drawCircle(mainCircle);
+    }
+
+//    public static void onTouchEvent(int x, int y) {
+//        mainCircle
+//    }
+
+//    public  void onTouchEvent(int x, int y) {
+//        mainCircle.moveMainCircleWhenTouchAt(x,y);
+//    }
+}
