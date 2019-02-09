570edc16a055918acab6fdf56a6006c1bcbcb088
constant-i <constant-i@yandex.ru>
2019-02-09 17:57:48 +0300

    добавили установку цвета круга в зависимости от размера, если больше главного, то враг, если меньше, то еда

--- EnemyCircle.java
+++ EnemyCircle.java
@@ -8,6 +8,7 @@ public class EnemyCircle extends SimpleCircle {
     public static final int FROM_RADIUS = 10;
     public static final int TO_RADIUS = 110;
     public static final int ENEMY_COLOR = Color.BLUE;
+    public static final int FOOD_COLOR = Color.rgb(0,255,0); // Color.GREEN;
 
 
     public EnemyCircle(int x, int y, int radius) {
@@ -22,5 +23,20 @@ public class EnemyCircle extends SimpleCircle {
         int radius = FROM_RADIUS + random.nextInt(TO_RADIUS - FROM_RADIUS);
         EnemyCircle enemyCircle = new EnemyCircle(x, y, radius);
         return enemyCircle;
+    }
+
+    public void setEnemyOrFoodColorDependsOn(MainCircle mainCircle) {
+        if (IsSmallerThen(mainCircle)){
+            setColor(FOOD_COLOR);
+        } else {
+            setColor(ENEMY_COLOR);
+        }
+    }
+
+    private boolean IsSmallerThen(SimpleCircle circle) {
+        if (radius < circle.radius) {
+            return true;
+        }
+        return false;
     }
 }
