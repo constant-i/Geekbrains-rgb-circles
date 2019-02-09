bb05cfa698160ba732cec1ed5a6a040d4f521567
constant-i <constant-i@yandex.ru>
2019-02-09 21:37:15 +0300

    - пытаюсь сделать разный знак у скорости

--- EnemyCircle.java
+++ EnemyCircle.java
@@ -9,7 +9,7 @@ public class EnemyCircle extends SimpleCircle {
     public static final int TO_RADIUS = 110;
     public static final int ENEMY_COLOR = Color.BLUE;
     public static final int FOOD_COLOR = Color.rgb(0,255,0); // Color.GREEN;
-    public static final int RANDOM_SPEED = 10;
+    public static final int RANDOM_SPEED = 20;
     private int dx;
     private int dy;
 
@@ -25,8 +25,17 @@ public class EnemyCircle extends SimpleCircle {
         Random random = new Random();
         int x = random.nextInt(GameManager.getWidth());
         int y = random.nextInt(GameManager.getHeight());
-        int dx = 1 + random.nextInt(RANDOM_SPEED);
-        int dy = 1 + random.nextInt(RANDOM_SPEED);
+        int dx;
+        int dy;
+
+        int forSpeed = 1 + random.nextInt(RANDOM_SPEED);
+        if (forSpeed <= (RANDOM_SPEED/2)) dx = (forSpeed - (RANDOM_SPEED/2));
+        else dx = (forSpeed * (-1));
+
+        forSpeed = 1 + random.nextInt(RANDOM_SPEED);
+        if (forSpeed <= (RANDOM_SPEED/2)) dy = (forSpeed - (RANDOM_SPEED/2));
+        else dy = (forSpeed * (-1));
+
         int radius = FROM_RADIUS + random.nextInt(TO_RADIUS - FROM_RADIUS);
         EnemyCircle enemyCircle = new EnemyCircle(x, y, radius, dx, dy);
         return enemyCircle;
