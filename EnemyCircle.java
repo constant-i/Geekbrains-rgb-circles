19aeb66ea723d12f75c3045dc18d478e18f4278c
constant-i <constant-i@yandex.ru>
2019-02-09 21:13:40 +0300

    - добавлены поля скорости по x и y - добавлена случайная инициализация скорости - добавлен метод движения на один шаг по x и y

--- EnemyCircle.java
+++ EnemyCircle.java
@@ -9,18 +9,26 @@ public class EnemyCircle extends SimpleCircle {
     public static final int TO_RADIUS = 110;
     public static final int ENEMY_COLOR = Color.BLUE;
     public static final int FOOD_COLOR = Color.rgb(0,255,0); // Color.GREEN;
+    public static final int RANDOM_SPEED = 10;
+    private int dx;
+    private int dy;
 
 
-    public EnemyCircle(int x, int y, int radius) {
+    public EnemyCircle(int x, int y, int radius, int dx, int dy) {
         super(x, y, radius);
+        this.dx = dx;
+        this.dy = dy;
+
     }
 
     public static EnemyCircle getRandomCircle() {
         Random random = new Random();
         int x = random.nextInt(GameManager.getWidth());
         int y = random.nextInt(GameManager.getHeight());
+        int dx = 1 + random.nextInt(RANDOM_SPEED);
+        int dy = 1 + random.nextInt(RANDOM_SPEED);
         int radius = FROM_RADIUS + random.nextInt(TO_RADIUS - FROM_RADIUS);
-        EnemyCircle enemyCircle = new EnemyCircle(x, y, radius);
+        EnemyCircle enemyCircle = new EnemyCircle(x, y, radius, dx, dy);
         return enemyCircle;
     }
 
@@ -37,5 +45,10 @@ public class EnemyCircle extends SimpleCircle {
             return true;
         }
         return false;
+    }
+
+    public void moveOneStep() {
+        x += dx;
+        y += dy;
     }
 }
