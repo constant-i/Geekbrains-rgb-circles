712453104bfc19b1e93b1fd3bab6ba1c5f6afe56
constant-i <constant-i@yandex.ru>
2019-02-09 21:45:40 +0300

    другой способ задавать скорость

--- EnemyCircle.java
+++ EnemyCircle.java
@@ -9,7 +9,7 @@ public class EnemyCircle extends SimpleCircle {
     public static final int TO_RADIUS = 110;
     public static final int ENEMY_COLOR = Color.BLUE;
     public static final int FOOD_COLOR = Color.rgb(0,255,0); // Color.GREEN;
-    public static final int RANDOM_SPEED = 20;
+    public static final int RANDOM_SPEED = 10;
     private int dx;
     private int dy;
 
@@ -29,12 +29,12 @@ public class EnemyCircle extends SimpleCircle {
         int dy;
 
         int forSpeed = 1 + random.nextInt(RANDOM_SPEED);
-        if (forSpeed <= (RANDOM_SPEED/2)) dx = (forSpeed - (RANDOM_SPEED/2));
-        else dx = (forSpeed * (-1));
+        if (forSpeed <= (RANDOM_SPEED/2)) dx = forSpeed;
+        else dx = -forSpeed;
 
         forSpeed = 1 + random.nextInt(RANDOM_SPEED);
-        if (forSpeed <= (RANDOM_SPEED/2)) dy = (forSpeed - (RANDOM_SPEED/2));
-        else dy = (forSpeed * (-1));
+        if (forSpeed <= (RANDOM_SPEED/2)) dy = forSpeed;
+        else dy = -forSpeed;
 
         int radius = FROM_RADIUS + random.nextInt(TO_RADIUS - FROM_RADIUS);
         EnemyCircle enemyCircle = new EnemyCircle(x, y, radius, dx, dy);
