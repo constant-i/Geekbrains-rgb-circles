ecf7dfc171487a4699297f8bbabc0aeab8d9bd62
constant-i <constant-i@yandex.ru>
2019-02-09 17:20:52 +0300

    задаём главному кругу цвет Красный (патамучта наш цвет - КРАСНЫЙ)

--- MainCircle.java
+++ MainCircle.java
@@ -1,11 +1,15 @@
 package com.hfad.rgbcircles;
 
+import android.graphics.Color;
+
 public class MainCircle extends SimpleCircle {
     public static final int INIT_RADIUS = 50;
     public static final int MAIN_SPEED = 30;
+    public static final int OUR_COLOR = Color.RED;
 
     public MainCircle(int x, int y) {
         super(x, y, INIT_RADIUS);
+        setColor(OUR_COLOR);
     }
 
 
