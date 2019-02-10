dcebf750efc66ab30265f953cd48e9bc53b7fd0a
constant-i <constant-i@yandex.ru>
2019-02-10 22:00:12 +0300

    в MyActivity добавленно отключение верней плашки с названием игры

--- MyActivity.java
+++ MyActivity.java
@@ -1,13 +1,27 @@
 package com.hfad.rgbcircles;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MyActivity extends AppCompatActivity {
+public class MyActivity extends Activity /*AppCompatActivity*/ {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_my);
     }
+
+    /*@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_my);
+    }*/
+
 }
